9354c15a4ad7db2c2d95b5e44f680825e6d1baf2
thelonewolf123 <harishkumar.vellore@gmail.com>
2020-11-20 22:59:52 +0530

    added .gitignore file

--- client/face_recognizer.py
+++ client/face_recognizer.py
@@ -1,106 +1,119 @@
-import face_recognition
+import json
+import threading
+import time
+
 import cv2
+import face_recognition
 import numpy as np
+import requests
+import wget
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+SERVER_URL = 'http://127.0.0.1:5000/'
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+def check_server():
+    while True:
+        url = SERVER_URL+'get_data'
+        req = requests.get(url).text
+        print('send request to the server')
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+        with open('data.json','w') as json_data:
+            json_data.write(req)
 
-# Load a sample picture and learn how to recognize it.
-lonewolf_image = face_recognition.load_image_file("lonewolf.png")
-lonewolf_face_encoding = face_recognition.face_encodings(lonewolf_image)[0]
+        time.sleep(10)
 
-# Load a second sample picture and learn how to recognize it.
-# biden_image = face_recognition.load_image_file("biden.jpg")
-# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+def face_recognizer():
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    lonewolf_face_encoding,
-]
-known_face_names = [
-    "Harish Kumar K",
-]
+    video_capture = cv2.VideoCapture(0)
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+    # Load a sample picture and learn how to recognize it.
+    known_face_encodings = []
+    known_face_names = []
+    with open('data.json') as json_data:
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+        content = json.loads(json_data.readlines())
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        for data in content:
+            person_image = face_recognition.load_image_file(data['file'])
+            person_face_encoding = face_recognition.face_encodings(person_image)[0]
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+            known_face_encodings.append(person_face_encoding)
+            known_face_names.append(data['name'])
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, face_locations)
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(
-                known_face_encodings, face_encoding)
-            name = "Unknown"
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(
-                known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-            face_names.append(name)
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(
+                rgb_small_frame, face_locations)
 
-    process_this_frame = not process_this_frame
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(
+                    known_face_encodings, face_encoding)
+                name = "Unknown"
+                
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+                face_names.append(name)
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        process_this_frame = not process_this_frame
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35),
-                      (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6),
-                    font, 1.0, (255, 255, 255), 1)
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35),
+                        (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),
+                        font, 1.0, (255, 255, 255), 1)
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    fr_thread = threading.Thread(target=face_recognizer)
+    cs_thread = threading.Thread(target=check_server)
+    
+    #start the threads
+    fr_thread.start()
+    cs_thread.start()
+    
+    # join the threads
+    fr_thread.join()
+    cs_thread.join()
