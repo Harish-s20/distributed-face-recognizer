bb245661722e74b0aa8c10b3ba75f213d18461a8
thelonewolf123 <harishkumar.vellore@gmail.com>
2020-11-20 21:56:03 +0530

    interfaced with sqlalchemy

--- server/server.py
+++ server/server.py
@@ -1,29 +1,51 @@
-from flask import Flask, render_template, redirect, request, jsonify, send_from_directory, flash
-from werkzeug.utils import secure_filename
-
 import os
 import uuid
+from datetime import datetime
+
+from flask import Flask, flash, jsonify
+from flask import redirect, render_template
+from flask import request, send_from_directory
+
+from dataclasses import dataclass
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.curdir, 'uploads')
+app.config['SECRET_KEY'] = "WGRwwA>L<](]c&z^umkHhC78?^(/ws'7"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///face_recognizer.db'
+
+db = SQLAlchemy(app)
+
+@dataclass
+class Person(db.Model):
+    id: int
+    name: str
+    file: str
+    disc: str
+    date: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    file = db.Column(db.String(100))
+    disc = db.Column(db.String(500))
+    date = db.Column(db.DateTime)
 
 
 @app.route('/')
 def index():
-    photo_list = []
-    for data in os.listdir('./uploads'):
-        photo_list.append({"person":data})
-    
-    return render_template('index.html',photo_list=photo_list)
+    persons = Person.query.all()
+
+    return render_template('index.html', photo_list=persons)
 
 
-@app.route('/photots')
+@app.route('/get_data')
 def photots():
-    photo_list = []
-    for data in os.listdir('./uploads'):
-        photo_list.append(data)
-
-    return jsonify(photo_list)
+    persons = Person.query.all()
+    print(persons)
+    return jsonify(persons)
 
 
 @app.route('/upload', methods=["POST"])
@@ -34,18 +56,28 @@ def upload_photo():
         return redirect(request.url)
 
     file = request.files['person']
+    name = request.form['name']
     discription = request.form['disc']
+    filename = str(uuid.uuid4()) + '.' + secure_filename(file.filename).split('.')[-1]
     print(discription)
     if file.filename == '':
         flash('No selected file')
         return redirect(request.url)
     if file:
         # filename = secure_filename(file.filename)
-        filename = str(uuid.uuid4())+'.'+secure_filename(file.filename).split('.')[-1]
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    person = Person(name=name, disc=discription,
+                    file=filename, date=datetime.now())
+    db.session.add(person)
+    db.session.commit()
 
     return redirect('/')
 
+@app.route('/view/<int:table_id>')
+def view_person(table_id):
+    person = Person.query.get_or_404(table_id)
+    return render_template('detail-view.html',person=person)
 
 @app.route('/uploads/<path:path>')
 def get_photo(path):
